ebc71b7a09ec3a776ebf92025e8670b1138378eb
impala <dla4051@naver.com>
2022-11-17 14:12:05 +0900

    convert layout of login page form flex to grid

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,33 @@
-import './Login.css';
-import { useState } from 'react';
+import styles from "./Login.module.css";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
-import axios from 'axios';
+import axios from "axios";
+import owl from './img/owl.png'
+import leftimg from './img/loginleft.png'
+import rightimg from './img/loginright.png'
 
 /*
   해야되는거
   3. 시작버튼 누르면 api호출
  */
 function Login() {
-  const REGISTER_URL = "#"
-  const LOGIN_API_URL = "https://890a-121-157-55-117.jp.ngrok.io"
+  const REGISTER_URL = "#";
+  const LOGIN_API_URL = "https://0284-211-63-140-94.jp.ngrok.io";
 
   const [inputField, setInput] = useState({
     idInput: "",
     passwordInput: "",
-  })
+  });
 
   const [activeBorder, setActiveBorder] = useState({
     idBorder: false,
-    passwordBorder : false,
+    passwordBorder: false,
   });
-  
 
   const { idInput, passwordInput } = inputField;
 
-  const { idBorder, passwordBorder} = activeBorder;
+  const { idBorder, passwordBorder } = activeBorder;
 
   const token = "";
 
@@ -36,7 +38,7 @@ function Login() {
     });
   };
 
-  const handleFocusBorder = border => {
+  const handleFocusBorder = (border) => {
     setActiveBorder({
       ...activeBorder,
       [border]: true,
@@ -44,7 +46,7 @@ function Login() {
   };
 
   const handleBlurBorder = (border, field) => {
-    if(!field){
+    if (!field) {
       setActiveBorder({
         ...activeBorder,
         [border]: false,
@@ -53,80 +55,102 @@ function Login() {
   };
 
   const handleLogin = () => {
-    axios.post(LOGIN_API_URL, {username: idInput, password: passwordInput})
-    .then((data)=>{
-      if(data.status === 200){
-        token = data.data.token
-        //마이페이지로ㄱㄱ
-      }
-      else{
-        //잘못입력함ㅋ
-      }
-    })
-    .catch(()=>{
-      //서버이상함ㅅㄱ
-    })
-  }
+    axios
+      .post(LOGIN_API_URL, { username: idInput, password: passwordInput })
+      .then((data) => {
+        if (data.status === 200) {
+          let token = data.data.token;
+          //마이페이지로ㄱㄱ
+        } else {
+          //잘못입력함ㅋ
+        }
+      })
+      .catch(() => {
+        //서버이상함ㅅㄱ
+      });
+  };
 
   return (
-    <div className="container">
-
-      <div className='left'>
-        <img src="img/loginleft.png"></img>
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img src={leftimg}></img>
       </div>
 
-      <div className="login-content">
-
-        <form>
-
-          <div className='img-div'>
-            <img src="img/dr_eung.png" />
+      <div className={styles.center}>
+        <div className={styles.innercontainer}>
+          <div
+            className={styles.logoimg}
+          >
+            <img src={owl}></img>
           </div>
-          
-          <h2 className="title">엉박사</h2>
 
-          <div className={ idBorder ? "input-div one focus" : "input-div one"}>
-            <FontAwesomeIcon icon={faUser} className='i'/>
-            <div className="div">
-              <h5>아이디</h5>
-              <input 
-                type="text" 
-                className="input"
-                onChange={(e) => handleInput('idInput', e.target.value)}
-                onFocus={() => handleFocusBorder('idBorder')}
-                onBlur={(e) => handleBlurBorder('idBorder', e.target.value)}
-              />
+          <div className={styles.아래}>
+            <div className={styles.inputcontainer}>
+              <div className={styles.title}>
+                <h2>엉박사</h2>
+              </div>
+              <div className={styles.idInput}>
+                <div
+                  className={
+                    idBorder
+                      ? `${styles.inputdiv} ${styles.one} ${styles.focus}`
+                      : `${styles.inputdiv} ${styles.one} `
+                  }
+                >
+                  <FontAwesomeIcon icon={faUser} className={styles.i} />
+                  <div className={styles.div}>
+                    <h5>아이디</h5>
+                    <input
+                      type="text"
+                      onChange={(e) => handleInput("idInput", e.target.value)}
+                      onFocus={() => handleFocusBorder("idBorder")}
+                      onBlur={(e) =>
+                        handleBlurBorder("idBorder", e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.pwInput}>
+                <div
+                  className={
+                    passwordBorder
+                      ? `${styles.inputdiv} ${styles.pass} ${styles.focus}`
+                      : `${styles.inputdiv} ${styles.pass} `
+                  }
+                >
+                  <FontAwesomeIcon icon={faLock} className={styles.i} />
+                  <div className={styles.div}>
+                    <h5>비밀번호</h5>
+                    <input
+                      type="password"
+                      onChange={(e) =>
+                        handleInput("passwordInput", e.target.value)
+                      }
+                      onFocus={() => handleFocusBorder("passwordBorder")}
+                      onBlur={(e) =>
+                        handleBlurBorder("passwordBorder", e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.btns}>
+                <a href={REGISTER_URL} className={styles.register}>
+                  회원가입
+                </a>
+                <btn className={styles.btn} onClick={() => handleLogin()}>
+                  시작하기
+                </btn>
+              </div>
             </div>
           </div>
-
-          <div className={ passwordBorder ? "input-div pass focus" : "input-div pass"}>
-            <FontAwesomeIcon icon={faLock} className='i'/>
-            <div className="div">
-              <h5>비밀번호</h5>
-                <input 
-                  type="password" 
-                  className="input"
-                  onChange={(e) => handleInput('passwordInput', e.target.value)}
-                  onFocus={() => handleFocusBorder('passwordBorder')}
-                  onBlur={(e) => handleBlurBorder('passwordBorder', e.target.value)}
-                />
-            </div>
-          </div>
-
-          <a href={ REGISTER_URL }>회원가입</a>
-          <input 
-            type="submit" 
-            className="btn" 
-            value="시작하기" 
-            style={{fontFamily:'Miwonfont', letterSpacing: '3px'}}
-            onClick={()=>handleLogin()} />
-        </form>
+        </div>
       </div>
-
-      <div className='right'>
-        <img src="img/loginright.png"></img>
+      <div className={styles.right}>
+        <img src={rightimg}></img>
       </div>
-
     </div>
   );
 }
