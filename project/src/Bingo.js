4963738e17433d4b743f70b5535935615c56ed0b
CHAN YOUNG <lms062111@naver.com>
2022-12-02 15:42:29 +0900

    progress total 단원 수 반영

---
+++ project/src/Bingo.js
@@ -0,0 +1,8 @@
+import styles from "./Bingo.module.css";
+import { useState, Component, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+function Bingo(){
+
+}
